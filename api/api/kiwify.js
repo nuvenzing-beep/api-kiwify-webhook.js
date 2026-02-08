3d4a9136b5990f88b14ca5f2ab46dec77279a3db
nuvenzing-beep <nuvenzing@gmail.com>
2026-02-08 14:34:04 -0300

    Delete api/api directory

--- api/api/kiwify.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
-  }
-
-  console.log("Webhook da Kiwify recebido");
-
-  return res.status(200).json({ ok: true });
-}
